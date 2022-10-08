6ec42f6c2ec68ff0d0c089f6167953e969a2fa2e
RolaLah <112093396+RolaLah@users.noreply.github.com>
2022-10-08 12:50:26 +0300

    Update adverb.dart

--- lib/screens/adverb.dart
+++ lib/screens/adverb.dart
@@ -41,7 +41,7 @@ class _AdverbState extends State<Adverb> with TickerProviderStateMixin {
           )
         ],
       ),
-      drawer: Drawer(),
+      drawer: DrawerScreen(),
       body: ListView(
         children: [
           Container(
