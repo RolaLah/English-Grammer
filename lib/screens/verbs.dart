a7770509ff920b50aec48ea3f0ecda93158113e7
RolaLah <112093396+RolaLah@users.noreply.github.com>
2022-10-08 12:55:28 +0300

    Update verbs.dart

--- lib/screens/verbs.dart
+++ lib/screens/verbs.dart
@@ -41,7 +41,7 @@ class _VerbsState extends State<Verbs> with TickerProviderStateMixin {
           )
         ],
       ),
-      drawer: Drawer(),
+      drawer: DrawerScreen(),
       body: ListView(
         children: [
           Container(
